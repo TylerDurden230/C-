07dd588b344faa431422340e66566fbc4978408c
Tylerdurden230 <75187191+TylerDurden230@users.noreply.github.com>
2021-11-02 23:35:44 +0100

    Add files via upload

--- CPP_00/ex02/Account.cpp
+++ CPP_00/ex02/Account.cpp
@@ -17,8 +17,8 @@ Account::Account(int initial_deposit):_accountIndex(getNbAccounts()), _amount(in
 	_totalAmount += initial_deposit;
 	_displayTimestamp();
 	std::cout << "index:" << this->_accountIndex
-			<< ";amount:" << this->_amount
-			<< ";created" << std::endl;
+	<< ";amount:" << this->_amount
+	<< ";created" << std::endl;
 }
 
 Account::~Account(void)
@@ -59,25 +59,53 @@ int	Account::getNbWithdrawals( void )
 
 void	Account::displayAccountsInfos( void )
 {
-
-}
-
-void	Account::makeDeposit( int deposit )
-{
-
-}
-    
-bool	Account::makeWithdrawal( int withdrawal )
-{
-
-}
-
-int		Account::checkAmount( void ) const
-{
-
+    _displayTimestamp();
+	std::cout << "accounts:" << _nbAccounts
+	<< ";total:" << _totalAmount
+	<< ";deposits:" << _totalNbDeposits
+	<< ";withdrawals:" << _totalNbWithdrawals << std::endl;
 }
 
 void	Account::displayStatus( void ) const
 {
+    _displayTimestamp();
+	std::cout << "index:" << this->_accountIndex
+	<< ";amount:" << this->_amount
+	<< ";deposits:" << this->_nbDeposits
+	<< ";withdrawals:" << this->_nbWithdrawals << std::endl;
+}
 
+void	Account::makeDeposit( int deposit )
+{
+    _totalNbDeposits++;
+	this->_nbDeposits++;
+	_totalAmount += deposit;
+	this->_amount+= deposit;
+	_displayTimestamp();
+	std::cout << "index:" << this->_accountIndex
+	<< ";p_amount:" << this->_amount - deposit
+	<< ";deposit:" << deposit
+	<< ";amount:" << this->_amount
+	<< ";nb_deposits:" << this->_nbDeposits << std::endl;
+}
+    
+bool	Account::makeWithdrawal( int withdrawal )
+{
+    _displayTimestamp();
+	std::cout << "index:" << this->_accountIndex
+	<< ";p_amount:" << this->_amount
+	<< ";withdrawal:";
+	if (withdrawal > this->_amount)
+    {
+	    std::cout << "refused" << std::endl;
+	    return (false);
+    }
+    _totalNbWithdrawals++;
+	this->_nbWithdrawals++;
+	std::cout << withdrawal
+	<< ";amount:" << this->_amount - withdrawal
+	<< ";nb_withdrawals:" << this->_nbWithdrawals << std::endl;
+	_totalAmount -= withdrawal;
+	this->_amount -= withdrawal;
+	return (true);
 }
