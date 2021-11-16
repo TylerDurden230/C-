e072cadb3c5ad3841918683ea38c0092746ab354
Giordano alessandro Carbonell <gcarbone@e3r6p5.42roma.it>
2021-11-16 18:54:49 +0100

    rifatto piu' semplice

--- CPP_01/ex06/main.cpp
+++ CPP_01/ex06/main.cpp
@@ -1,51 +1,38 @@
 #include  "Karen.hpp"
 
-
-bool filter(std::string level,std::string lvlStr)
-{
-    size_t pos , end;
-    bool valid;
-
-    valid = false;
-    pos = lvlStr.find(level, 0);
-    if (pos != std::string::npos)
-    {
-        end = pos + level.length();
-        if (level.find(",", 0) == std::string::npos && (end == lvlStr.length()  || lvlStr.at(end) == ','))
-            valid = true;
-    }
-    return valid;
-}
-
 int main(int ac, char *argv[])
 {
     Karen karen;
-    std::string lvlStr = "DEBUG,INFO,WARNING,ERROR";
-    enum levels {
-        DEBUG = 0,
-        INFO = 6,
-        WARNING = 11,
-        ERROR = 19
-    };
-    size_t pos = -1;
-
+    std::string lvlArr  [] = {"DEBUG","INFO","WARNING","ERROR"};
     if (ac == 2)
     {
         std::string level(argv[1]);
-        if (filter(level,lvlStr))
-            pos = lvlStr.find(level, 0);
-        switch (pos)
+        for(int i = 0; i < 4; i++)
         {
-            case DEBUG :
-            case INFO :
-            case WARNING :
-            case ERROR :
-                karen.complain(argv[1]);
-            break;
-            default:
-                std::cout << "[ Probably complaining about insignificant problems ]" << std::endl;
-            break;
+            if (level == lvlArr[i])
+            {
+                switch (i)
+                {
+                    case 0 :
+                        std::cout << "[ DEBUG ]" << std::endl;
+                    break;
+                    case 1 :
+                        std::cout << "[ INFO ]" << std::endl;
+                    break;
+                    case 2 :
+                        std::cout << "[ WARNING ]" << std::endl;
+                    break;
+                    case 3 :
+                        std::cout << "[ ERROR ]" << std::endl;
+                    break;
+                    default:
+                    break;
+                }
+                karen.complain(level);
+                return 0;
+            }
         }
+        std::cout << "[ Probably complaining about insignificant problems ]" << std::endl;
     }
     else {
         std::cout << "Wrong arguments number. Shame on You!!!!" << std::endl;
