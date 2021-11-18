a068608fa1f70162b30328e9ff1a5dacae419566
Flavio D'agnese <fd-agnes@e3r7p1.42roma.it>
2021-11-18 17:38:44 +0100

    Refresh

---
+++ CPP_02_not_ours/ex01/Fixed.hpp
@@ -0,0 +1,26 @@
+
+
+#pragma once
+
+#include <cmath>
+#include <iostream>
+
+class Fixed
+{
+	private:
+		int raw;
+		static const int Q;
+	public:
+		Fixed();
+		~Fixed();
+		Fixed(const Fixed &original);
+		Fixed(const int num);
+		Fixed(const float num);
+		Fixed	&operator=(const Fixed &original);
+		int		getRawBits(void) const;
+		void	setRawBits(int const raw);
+		float	toFloat() const;
+		int		toInt() const;
+};
+
+std::ostream& operator<<(std::ostream& os, const Fixed &fx);
