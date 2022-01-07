c3f4c200799632b43cd6a7253c298ab27514a1f9
Tylerdurden230 <75187191+TylerDurden230@users.noreply.github.com>
2022-01-07 20:05:33 +0100

    Add files via upload

---
+++ CPP_08/ex00/easyfind.hpp
@@ -0,0 +1,36 @@
+#ifndef EASYFIND_HPP
+#define EASYFIND_HPP
+
+#include <iostream>
+#include <algorithm>
+
+# define RESET			"\033[0m"
+# define RED			"\033[31m"				/* Red */
+# define BLUE			"\033[34m"				/* Blue */
+
+class ElementNotFound: public std::exception
+{
+public:
+	virtual const char* what() const throw()
+	{
+		return "Element not found";
+	}
+};
+
+template<typename T>
+void	easyfind(T & t, int i)
+{
+	typename T::const_iterator iter;
+	iter = std::find(t.begin(), t.end(), i);
+	try {
+		if (iter == t.end())
+			throw (ElementNotFound());
+	}
+	catch (std::exception & exception) {
+		std::cout << RED << exception.what() << RESET << std::endl;
+		return ;
+	}
+	std::cout << BLUE << "Element found: " << *iter << RESET << std::endl;
+}
+
+#endif
---
+++ CPP_08/ex01/main.cpp
@@ -0,0 +1,60 @@
+#include "span.hpp"
+
+int main()
+{
+	try
+	{
+		Span myarr(5);
+		Span second_arr;
+		myarr.addNumber(5);
+		myarr.addNumber(3);
+		myarr.addNumber(17);
+		myarr.addNumber(9);
+		myarr.addNumber(11);
+
+		std::cout << myarr.shortestSpan() << std::endl;
+		std::cout << myarr.longestSpan() << std::endl;
+
+		Span pippo;
+		second_arr = pippo;
+		std::cout << second_arr.longestSpan() << std::endl;
+	}
+	catch(std::exception& e)
+	{
+		std::cerr << e.what() << '\n';
+	}
+	
+	try
+	{
+		Span myarr(2);
+		Span second_arr;
+		myarr.addNumber(13);
+		myarr.addNumber(36);
+		myarr.addNumber(21);
+
+	}
+	catch(std::exception& e)
+	{
+		std::cerr << e.what() << '\n';
+	}
+	try
+	{
+		int n;
+		std::cout << "\nNote that a number higher than 10000 can take a while\nInsert the number   ";
+		std::cin >> n;
+        if (n > 10000)
+            std::cout << "\nniente...sei de coccio\n";
+		Span myarr(n);
+		srand(time(0));
+		for (int i = 0; i < n; i++)
+			myarr.addNumber(rand() % rand());
+
+		std::cout << myarr.shortestSpan() << std::endl;
+		std::cout << myarr.longestSpan() << std::endl;
+	}
+	catch(std::exception& e)
+	{
+		std::cerr << e.what() << '\n';
+	}
+	
+}
